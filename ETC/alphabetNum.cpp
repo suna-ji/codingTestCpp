f8ecb4de4d0a7da6db88c230162f0540beb32165
suna <jisuna314@gmail.com>
2020-09-02 21:40:08 +0900

    alphabet Number - 10808

---
+++ ETC/alphabetNum.cpp
@@ -0,0 +1,17 @@
+// 알파벳 개수 - 10808
+#include <iostream>
+using namespace std;
+int alphabet[26];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string word;
+    cin >> word;
+   
+    for(int i = 0; i < word.length(); i++){
+        alphabet[(int)word[i]-97]++;
+    }
+    for(int i = 0; i < 26; i++){
+        cout << alphabet[i] << " ";
+    }
+}
