ae696968d29871d0a503934c81cf3178215739ee
suna <jisuna314@gmail.com>
2020-09-20 11:39:12 +0900

    잃어버린 괄호 - 1541

--- Greedy/missingClosure.cpp
+++ Greedy/missingClosure.cpp
@@ -1,3 +1,4 @@
+// 잃어버린 괄호 - 1541
 #include <iostream>
 #include <string>
 using namespace std;
@@ -11,7 +12,6 @@ int main(){
 
     for(int i = 0; i <= input.length(); i++){
         if(input[i] == '+' || input[i] == '-' || input[i] == '\0'){
-            cout << temp << endl;
             if(minus){
                 result -= stoi(temp);
             }
@@ -27,3 +27,5 @@ int main(){
     }
     cout << result;
 }
+// -연산과 + 연산이 번갈아 나오기 때문에 -가 한번 나오면 그 뒤로는 그냥 다 빼주면 된다.
+// 
