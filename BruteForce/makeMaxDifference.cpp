6a54af87d0b3dcae2b0db14a5eb746ca6bb63b3a
suna <jisuna314@gmail.com>
2020-09-26 15:17:53 +0900

    차이를 최대로 - 10819(재귀 순열)

--- BruteForce/makeMaxDifference.cpp
+++ BruteForce/makeMaxDifference.cpp
@@ -1,22 +1,46 @@
 // 차이를 최대로 - 10819
+// 출처: https://yabmoons.tistory.com/98
+// 주어진 수열의 경우를 모두 구해서 식을 계산하고 최댓값을 찾는 문제
 #include <iostream>
+#include <vector>
+#include <cmath> // abs 절대값!
 #include <algorithm>
 using namespace std;
 int arr[8];
+bool selectArr[8];
+vector<int> v;
+int n;
 int maxResult = -999999;
-void func(int idx, int result, int cnt){
-    if(cnt == n/2){
-        maxResult = max(maxResult, result);
+void calculate(){
+    int result = 0;
+    for(int i = 0; i < v.size()-1; i++){
+        result += abs(v[i]-v[i+1]);
+    }
+    maxResult = max(maxResult, result);
+}
+void dfs(int cnt){
+    if(cnt == n){ // 어차피 baseCondition으로 와야 return되어 dfs함수가 끝나니까 vector섞이고 이런거 생각안해도 된다. 
+        calculate();
         return;
     }// base condition
-    func(idx+2,,cnt++)
+    for(int i = 0; i < n; i++){
+        if(selectArr[i] == true) continue;
+        selectArr[i] = true;
+        v.push_back(arr[i]);
+        dfs(cnt+1);
+        v.pop_back();
+        selectArr[i] = false;
+    }
 }
 
 int main(){
-    int n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n; // 3 <= n <= 8
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    func(0, 0, 0);
+    // result = 0, cnt = 0;
+    dfs(0);
+    cout << maxResult << "\n";
 }
