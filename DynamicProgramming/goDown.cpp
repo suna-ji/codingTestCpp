736ecbfc4d016ceaf36b88a89d99fcf6e80f5549
지선아 <jisuna314@gmail.com>
2020-10-13 16:16:38 +0900

    :hammer: 내려가기 - 2096

--- DynamicProgramming/goDown.cpp
+++ DynamicProgramming/goDown.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 int N;
 int map[MAX][3];
-int maxMemo[MAX][3];
-int minMemo[MAX][3]; 
+int maxMemo[2][3];
+int minMemo[2][3]; 
 
 int main(){
     ios::sync_with_stdio(0);
@@ -25,19 +25,27 @@ int main(){
         minMemo[0][i] = map[0][i];
     }
     for(int i = 1; i < N; i++){
-       maxMemo[i][0] = max(maxMemo[i-1][0], maxMemo[i-1][1]) + map[i][0];
-       maxMemo[i][1] = max(max(maxMemo[i-1][0], maxMemo[i-1][1]), maxMemo[i-1][2]) + map[i][1];
-       maxMemo[i][2] = max(maxMemo[i-1][1], maxMemo[i-1][2]) + map[i][2];
+       maxMemo[1][0] = max(maxMemo[0][0], maxMemo[0][1]) + map[i][0];
+       maxMemo[1][1] = max(max(maxMemo[0][0], maxMemo[0][1]), maxMemo[0][2]) + map[i][1];
+       maxMemo[1][2] = max(maxMemo[0][1], maxMemo[0][2]) + map[i][2];
        
-       minMemo[i][0] = min(minMemo[i-1][0], minMemo[i-1][1]) + map[i][0];
-       minMemo[i][1] = min(min(minMemo[i-1][0], minMemo[i-1][1]), minMemo[i-1][2]) + map[i][1];
-       minMemo[i][2] = min(minMemo[i-1][1], minMemo[i-1][2]) + map[i][2];
+       minMemo[1][0] = min(minMemo[0][0], minMemo[0][1]) + map[i][0];
+       minMemo[1][1] = min(min(minMemo[0][0], minMemo[0][1]), minMemo[0][2]) + map[i][1];
+       minMemo[1][2] = min(minMemo[0][1], minMemo[0][2]) + map[i][2];
+
+       maxMemo[0][0] = maxMemo[1][0];
+       maxMemo[0][1] = maxMemo[1][1];
+       maxMemo[0][2] = maxMemo[1][2];
+
+       minMemo[0][0] = minMemo[1][0];
+       minMemo[0][1] = minMemo[1][1];
+       minMemo[0][2] = minMemo[1][2];
     }
-    int minScore = 99999;
+    int minScore = 999999999;
     int maxScore = 0;
     for(int i = 0; i < 3; i++){
-        maxScore = max(maxScore, maxMemo[N-1][i]);
-        minScore = min(minScore, minMemo[N-1][i]);
+        maxScore = max(maxScore, maxMemo[0][i]);
+        minScore = min(minScore, minMemo[0][i]);
     }
     cout << maxScore << " " << minScore;
 }
