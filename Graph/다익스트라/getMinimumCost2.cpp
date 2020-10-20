3275078d67b1f91549fb7c68dacab336c55bbd25
지선아 <jisuna314@gmail.com>
2020-10-20 19:45:46 +0900

    :sparkles: 11779 - 최소비용 구하기2

--- Graph/다익스트라/getMinimumCost2.cpp
+++ Graph/다익스트라/getMinimumCost2.cpp
@@ -1,57 +1,59 @@
-// 최소비용 구하기2 - 11779
-// PQ 참고!
-// 최소비용과 그 경로를 출력
-// Route[A] = B : A정점에 최소비용으로 도달하기 위해서는 b정점에서 와야한다.
-
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <queue>
-#define MAX 1000 + 2
-#define INF 999999999;
+#define MAX 1000 + 2 //도시의 개수는 1이상 1000이하
+#define INF 99999999
 using namespace std;
-int N; // 도시의 개수
-int M; // 버스의 개수
-int from, to;
-int dist[MAX];
 
-vector<pair<int, int> > V[MAX];
-vector<int> Route_V;
-priority_queue<pair<int, int> > PQ;
-
+int N, M; // 도시 버스
+int start, stop; // 출발지 도착지
+vector<pair<int, int> > v[MAX]; // 도착지와 비용쌍을 갖는 벡터배열
+int dist[MAX]; // dist[i] : i까지의 최소비용
+int route[MAX];
+vector<int> reversed_route;
+priority_queue<pair<int, int> > pq; // pq에서 하나씩 꺼내면서 계산할거다! COST와 정점정보둘다 필요! 
 int main(){
+    ios:: sync_with_stdio(0);
+    cin.tie(0);
+    // 빠른 입출력
     cin >> N >> M;
-    for(int i = 1; i <= N; i++){
-        dist[i] = INF;
-    }// dist(비용) 초기화
-
     int a, b, c;
     for(int i = 0; i < M; i++){
         cin >> a >> b >> c;
-        V[a].push_back(make_pair(b, c));
-    }//
-
-    cin >> from >> to;
-    PQ.push(make_pair(0, from)); 
-    dist[from] = 0;
-    while(PQ.empty() == 0){
-        int Cost = -PQ.top().first; // 최소힙으로 구현하기 위함
-
-        int Cur = PQ.top().second;
-        PQ.pop();
-        for(int i = 0; i < V[Cur].size(); i++){
-            int Next = V[Cur][i].first;
-            int nCost = V[Cur][i].second;
-
-            if(Dist[Next] > Cost + nCost){
-                Route[Next] = Cur;
-                Dist[Next] = Cost + nCost;
-                PQ.push(make_pair(-Dist[Next], Next));
+        v[a].push_back(make_pair(b, c));
+    }
+    cin >> start >> stop;
+    // 입력완료!
+    for(int i  = 1; i <= N; i++){ // 모든 도시에 대해서 최소비용 INF로 초기화
+        dist[i] = INF;
+    }
+    pq.push(make_pair(0, start));
+    dist[start] = 0;
+    // 초기화
+    while(!pq.empty()){
+        int cost = -pq.top().first; // 최소힙 구현 (가장 비용 적은거 구해야 하니까)
+        int current = pq.top().second;
+        pq.pop();
+        for(int i = 0; i < v[current].size(); i++){
+            int next = v[current][i].first;
+            int nextCost = v[current][i].second;
+            if(dist[next] > cost + nextCost){
+                dist[next] = cost + nextCost;
+                route[next] = current;
+                pq.push(make_pair(-dist[next], next)); // 넣을 때, -붙여야 최소힙 구현가능
             }
         }
     }
-    cout << Dist[End] << "\n";
-    int Temp = End;
-
-
-    
+    cout << dist[stop] << "\n";
+    // 경로 역추적
+    int temp = stop;
+    while(temp){
+        reversed_route.push_back(temp);
+        temp = route[temp];
+    }
+    cout << reversed_route.size() << "\n";
+    for(int i = reversed_route.size()-1; i >= 0; i--){
+        cout << reversed_route[i] << " ";
+    }
 }
